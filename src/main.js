ae71baa29481183bed4ac719182db7d611eb2b94
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2025-06-01 15:46:49 +0300

    Updated the main js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store/index";
+import router from "./router/router";
+import BaseSection from "./Base/BaseSection.vue";
+import BaseButton from "./Base/BaseButton.vue";
+const app = createApp(App);
+app.component("base-section", BaseSection);
+app.component("base-button", BaseButton);
 
-createApp(App).mount('#app')
+app.use(store);
+app.use(router);
+app.mount("#app");
