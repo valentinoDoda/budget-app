469744445f020fe0d4221ffee96fea4fd887361f
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2025-06-01 14:06:42 +0300

    I created the pages folder and the hero page of the app

---
+++ src/pages/HeroPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <base-section>
+    <div class="hero-section">
+      <h1>Take Control of Your Finances</h1>
+      <p>
+        Track your income and expenses with ease. Stay on top of your budget
+        with a clean and smart interface designed for everyday use.
+      </p>
+      <base-button
+        :mode="'main'"
+        style="margin: auto; width: max-content"
+        to="/sign"
+        >Start Managing Today</base-button
+      >
+    </div>
+  </base-section>
+</template>
+
+<style scoped>
+.hero-section {
+  margin-top: 90px;
+  text-align: center;
+}
+
+.hero-section h1 {
+  font-weight: medium;
+  color: var(--titles-color);
+  font-size: 50px;
+  margin-bottom: 30px;
+}
+
+.hero-section p {
+  font-size: 24px;
+  color: var(--description-color);
+  margin-bottom: 90px;
+}
+</style>
