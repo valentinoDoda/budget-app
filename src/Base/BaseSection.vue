8430f058a11eaf0b34b02113ee07d4caa86ddf41
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2025-06-01 14:21:02 +0300

    I created the base section which includes styling for the proper showing of other pages

---
+++ src/Base/BaseSection.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="section">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.section{
+    margin: auto;
+    max-width: 1136px;
+    padding: 10px 20px;
+}
+</style>
