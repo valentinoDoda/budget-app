9931afe0761f3f4d093610b900a99b58ce42d68f
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2025-06-01 15:43:04 +0300

    I created the move element to showcase the user movements and the move delails to present  the details for each movement

---
+++ src/components/MoveDetails.vue
@@ -0,0 +1,112 @@
+<template>
+  <base-section>
+    <div class="card">
+      <div class="date">date : 13/03/2025</div>
+      <div class="type expense">Income</div>
+      <div class="amount">$300</div>
+
+      <div class="field">
+        <div class="field-title">Title:</div>
+        <div class="field-value">Shoes</div>
+      </div>
+
+      <div class="field">
+        <div class="field-title">Description:</div>
+        <div class="field-value">Shoes about football shoes</div>
+      </div>
+
+      <div class="field">
+        <div class="field-title">Niche:</div>
+        <div class="field-value">Shopping</div>
+      </div>
+      <button @click="goBack" class="go-back">GO BACK</button>
+    </div>
+  </base-section>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.back();
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  width: 100%;
+}
+.card {
+  margin: auto;
+  background-color: #1e1e1e; /* Almost black */
+  color: #fff;
+  padding: 1.5rem;
+  width: 300px;
+  border-radius: 6px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.date {
+  font-size: 0.9rem;
+  text-align: center;
+  color: #aaa;
+}
+
+.income {
+  background-color: #708654;
+}
+
+.expense {
+  background-color: #682929;
+}
+
+.type {
+  text-align: center;
+  padding: 0.5rem 0;
+  font-size: 1.4rem;
+  font-weight: bold;
+  border-radius: 2px;
+}
+
+.amount {
+  text-align: center;
+  font-size: 1.8rem;
+  font-weight: bold;
+  margin-top: -0.5rem;
+}
+
+.field {
+  margin-top: 0.5rem;
+}
+
+.field-title {
+  font-weight: bold;
+  font-size: 1rem;
+  margin-bottom: 0.1rem;
+}
+
+.field-value {
+  color: #999;
+  font-size: 0.95rem;
+}
+
+.go-back {
+  margin-top: 2rem;
+  background-color: white;
+  color: #000;
+  padding: 0.7rem 2rem;
+  border: none;
+  border-radius: 4px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.go-back:hover {
+  background-color: #ddd;
+}
+</style>
---
+++ src/components/MoveElement.vue
@@ -0,0 +1,79 @@
+<template>
+  <li>
+    <div class="move_element">
+      <div class="move_type">
+        <div class="type_bar income"></div>
+      </div>
+      <div class="move_title">
+        <p>Title of the move</p>
+      </div>
+      <div class="move_value">$200</div>
+      <div class="move_date"></div>
+      <button>view more</button>
+    </div>
+  </li>
+  <li>
+    <div class="move_element">
+      <div class="move_type">
+        <div class="type_bar expense"></div>
+      </div>
+      <div class="move_title">
+        <p>Title of the move</p>
+      </div>
+      <div class="move_value">$200</div>
+      <div class="move_date"></div>
+      <button @click="viewDetails">view more</button>
+    </div>
+  </li>
+</template>
+<script>
+export default {
+    methods:{
+        viewDetails(){
+            this.$router.push({name:"MoveDetail", params:{detail: "2"}})
+        }
+    }
+}
+</script>
+<style scoped>
+li{
+    margin-top:23px;
+}
+button{
+    cursor: pointer;
+    background: none;
+    border: none;
+    font-size: 14px;
+    text-decoration: underline;
+    color: #cbcbdd;
+}
+.move_element{
+    color: white;
+    font-size: 18px;
+    text-align: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 6px;
+    background-color: #1a1f14;
+    padding: 10px;
+}
+
+.move_type{
+    height: 100%;
+}
+
+
+.income {
+  background-color: #5a6d43;
+}
+.expense {
+  background-color: #682929;
+}
+
+.type_bar {
+  border-radius: 50px;
+  height: 10px;
+  width: 10px;
+}
+</style>
