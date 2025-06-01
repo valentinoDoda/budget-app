4fca3042efe3d5e2890dd6068f7cafbebe2b8fe3
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2025-06-01 14:15:33 +0300

    I created a base button

---
+++ src/Base/BaseButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button :class="mode" v-if="type == 'btn'">
+    <slot></slot>
+  </button>
+  <router-link :class="mode" v-else>
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: ["mode", "type"],
+};
+</script>
+
+<style scoped>
+a {
+  text-align: center;
+  display: block;
+}
+
+.main {
+  color: white;
+  background-color: #5a6d43;
+}
+.second {
+  background-color: white;
+  color: #5a6d43;
+}
+.third {
+  color: #f4f6ef;
+  background-color: #171616;
+}
+.main,
+.second,
+.third {
+  cursor: pointer;
+  border: none;
+  font-weight: bold;
+  border-radius: 6px;
+  padding: 10px 20px;
+  font-size: 21px;
+}
+</style>
+>
